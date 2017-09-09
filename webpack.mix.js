8e97e9fc1089a53f6384eb9987156c8edf0d96a0
Guillem <guillem@disedit.com>
2017-09-09 20:27:25 +0200

    Add versoning to CSS and Javascript assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,13 +15,12 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .js('resources/assets/js/admin.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/admin.scss', 'public/css')
-   .options({
+   .sass('resources/assets/sass/admin.scss', 'public/css');
+
+mix.version();
+
+mix.options({
         postCss: [
             require('postcss-flexbugs-fixes')()
         ]
    });
-
-if (mix.config.inProduction) {
-  mix.version();
-}
