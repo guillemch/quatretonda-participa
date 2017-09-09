b534edf88db22c9dd3337d4dbf6d3ff4baff5eeb
Guillem <guillem@disedit.com>
2017-09-09 18:11:50 +0200

    Add truncate option to reset census command

--- app/Console/Commands/ResetCensus.php
+++ app/Console/Commands/ResetCensus.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use App\Edition;
+use App\Ballot;
 
 class ResetCensus extends Command
 {
@@ -12,7 +14,7 @@ class ResetCensus extends Command
      *
      * @var string
      */
-    protected $signature = 'census:reset {--edition=}';
+    protected $signature = 'census:reset {--edition=} {--truncate}';
 
     /**
      * The console command description.
@@ -41,7 +43,20 @@ class ResetCensus extends Command
         $editionId = $this->option('edition');
         $edition = ($editionId) ? Edition::where('id', $editionId)->first() : Edition::current();
 
+        $truncate = $this->option('truncate');
+
         $this->info('Reseting census on edition ' . $edition->name . ' ...');
+
+        if($truncate) {
+
+            Ballot::truncate();
+            DB::table('voters')->delete();
+            DB::unprepared("ALTER TABLE voters AUTO_INCREMENT = 1");
+
+            $this->info('Ballot and voter tables emptied');
+
+            return;
+        }
 
         foreach($edition->voters()->get() as $voter) {
             $voter->SMS_phone = '';
@@ -57,6 +72,9 @@ class ResetCensus extends Command
             $voter->user_agent = '';
 
             $voter->save();
+
+            // Delete their ballots
+            $voter->ballot()->delete();
         }
 
         $this->info('Census reset successfully.');
